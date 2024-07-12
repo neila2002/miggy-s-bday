2c70a301f4fff4a6a88c7e05be07a69de9536ee2
neila2002 <naila.benmesbah@gmail.com>
2024-07-12 14:34:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,27 +19,4 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
-
-    // Replace placeholders with actual content
-    const today = new Date();
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    const formattedDate = today.toLocaleDateString('en-US', options);
-
-    const placeholders = {
-        '[MIGGYYYY]': 'Your Friend',
-        '[TODAY JULY 14 2024]': formattedDate,
-        '[RIGHT NOW]': 'Anytime you want',
-        '[RIGHT HERE MFA]': 'In the comfort of your home',
-        '[Contact Information]': '123-456-7890',
-        'https://codes.rewardcodes.com/r2/1/HQxSRimHiXuYokE4bzsRbxU8zrDl6VUEBrZh271y7ik6illK5mKKm9H_ZrrIMsho': 'https://codes.rewardcodes.com/r2/1/HQxSRimHiXuYokE4bzsRbxU8zrDl6VUEBrZh271y7ik6illK5mKKm9H_ZrrIMsho'
-    };
-
-    const elements = document.querySelectorAll('body *');
-
-    elements.forEach(el => {
-        el.innerHTML = el.innerHTML.replace(
-            /\[(MIGGYYYY|TODAY JULY 14 2024|RIGHT NOW|RIGHT HERE MFA|Contact Information|https:\/\/codes.rewardcodes.com\/r2\/1\/HQxSRimHiXuYokE4bzsRbxU8zrDl6VUEBrZh271y7ik6illK5mKKm9H_ZrrIMsho)\]/gi,
-            match => placeholders[match]
-        );
-    });
 });
